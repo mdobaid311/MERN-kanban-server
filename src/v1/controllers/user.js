79e92ffa11ee708902bea3eb1d48dc2b490fe174
mdobaid311 <mdobaid311@gmail.com>
2022-08-18 12:38:14 +0530

    initial commit

---
+++ src/v1/controllers/user.js
@@ -0,0 +1,73 @@
+const User = require("../models/user.js");
+const CryptoJS = require("crypto-js");
+const jsonwebtoken = require("jsonwebtoken");
+
+exports.register = async (req, res) => {
+    const {
+        password
+    } = req.body
+
+    try {
+        req.body.password = CryptoJS.AES.encrypt(password, process.env.PASSWORD_SECRET_KEY)
+
+        const user = await User.create(req.body)
+        const token = jsonwebtoken.sign({
+            id: user._id
+        }, process.env.TOKEN_SECRET_KEY, {
+            expiresIn: "24h"
+        })
+        res.status(201).json({
+            user,
+            token
+        })
+    } catch (error) {
+        res.status(500).json(error)
+    }
+}
+
+exports.login = async (req, res) => {
+    const {
+        username,
+        password
+    } = req.body;
+
+    try {
+        const user = await User.findOne({
+            username
+        }).select('password username')
+
+        if (!user) {
+            return res.status(404).json({
+                errors: [{
+                    param: "username",
+                    msg: "Invalid username or password"
+                }]
+            })
+        }
+        const decryptedPassword = CryptoJS.AES.decrypt(user.password, process.env.PASSWORD_SECRET_KEY).toString(CryptoJS.enc.Utf8)
+
+        if (decryptedPassword !== password) {
+            return res.status(404).json({
+                errors: [{
+                    param: "username",
+                    msg: "Invalid username or password"
+                }]
+            })
+        }
+        user.password = undefined
+
+        const token = jsonwebtoken.sign({
+            id: user._id
+        }, process.env.TOKEN_SECRET_KEY, {
+            expiresIn: "24h"
+        })
+
+        res.status(200).json({
+            user,
+            token
+        })
+
+    } catch {
+
+    }
+}
